ce78ae44ef1d1fd55410703feb2a439222a7bfae
Lori Corbani <Lori.Corbani@jax.org>
2000-06-19 13:55:33 +0000

    TR 1604

---
+++ monthly_reports.sh
@@ -0,0 +1,19 @@
+#!/bin/csh
+
+#
+# monthly_reports.sh
+#
+# Script to generate monthly qc reports.
+#
+# Usage: monthly_reports.sh
+#
+
+cd `dirname $0` && source Configuration
+
+umask 002
+
+cd monthly
+foreach i (`ls *.py`)
+$i
+end
+
