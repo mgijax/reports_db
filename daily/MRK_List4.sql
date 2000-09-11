56183da93b9d90fd36da4e85729a78d33ca6eb27
Lori Corbani <Lori.Corbani@jax.org>
2000-09-11 12:54:05 +0000

    TR 1930

--- daily/MRK_List4.sql
+++ daily/MRK_List4.sql
@@ -2,7 +2,7 @@ print ""
 print "Genetic Marker List (sorted by chromosome/excludes withdrawns)"
 print ""
 
-select a.accID "MGI Accession ID", m.chromosome "Chr", m.symbol "Symbol", substring(m.name, 1, 40) "Name"
+select a.accID "MGI Accession ID", m.chromosome "Chr", m.symbol "Symbol", m.name "Name"
 from MRK_Marker m, MRK_Chromosome c, MRK_Acc_View a
 where m._Species_key = 1
 and m._Marker_Status_key = 1
