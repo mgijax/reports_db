8d5430848e1461d0cfa40331d5be19e379c8fd26
Lori Corbani <Lori.Corbani@jax.org>
2001-06-19 15:52:31 +0000

    TR 2644

--- daily/MRK_List1.sql
+++ daily/MRK_List1.sql
@@ -12,9 +12,24 @@ select a.accID "MGI Accession ID", m.chromosome "Chr",
 ,m.symbol "Symbol", m.name "Name"
 from MRK_Marker m, MRK_Acc_View a, MRK_Offset o
 where m._Species_key = 1
+and m._Marker_Status_key = 1
 and m._Marker_key = a._Object_key
 and a.prefixPart = "MGI:"
 and a.preferred = 1
+and m._Marker_key = o._Marker_key
+and o.source = 0
+union
+select null, m.chromosome,
+"cM Position" =
+        case
+        when o.offset >= 0 then str(o.offset, 10, 2)
+        when o.offset = -999.0 then "       N/A"
+        when o.offset = -1.0 then "  syntenic"
+        end
+,m.symbol "Symbol", m.name "Name"
+from MRK_Marker m, MRK_Offset o
+where m._Species_key = 1
+and m._Marker_Status_key = 2
 and m._Marker_key = o._Marker_key
 and o.source = 0
 order by m.symbol
--- daily/MRK_List3.sql
+++ daily/MRK_List3.sql
@@ -1,15 +1,8 @@
-print ""
-print "Genetic Marker List (sorted by chromosome/includes withdrawns)"
-print ""
+set nocount on
+go
 
-select a.accID "MGI Accession ID", m.chromosome "Chr", 
-"cM Position" =
-        case
-        when o.offset >= 0 then str(o.offset, 10, 2)
-        when o.offset = -999.0 then "       N/A"
-        when o.offset = -1.0 then "  syntenic"
-        end
-, m.symbol "Symbol", m.name "Name"
+select c.sequenceNum, a.accID, m.chromosome, o.offset, m.symbol, m.name
+into #output
 from MRK_Marker m, MRK_Chromosome c, MRK_Acc_View a, MRK_Offset o
 where m._Species_key = 1
 and m._Species_key = c._Species_key
@@ -19,6 +12,32 @@ and a.prefixPart = "MGI:"
 and a.preferred = 1
 and m._Marker_key = o._Marker_key
 and o.source = 0
+union
+select c.sequenceNum, null, m.chromosome, o.offset, m.symbol, m.name
+from MRK_Marker m, MRK_Chromosome c, MRK_Offset o
+where m._Species_key = 1
+and m._Marker_Status_key = 2
+and m._Species_key = c._Species_key
+and m.chromosome = c.chromosome
+and m._Marker_key = o._Marker_key
 order by c.sequenceNum, m.symbol
+go
+
+set nocount off
+go
+
+print ""
+print "Genetic Marker List (sorted by chromosome/includes withdrawns)"
+print ""
+
+select o.accID "MGI Accession ID", o.chromosome "Chr", 
+"cM Position" =
+        case
+        when o.offset >= 0 then str(o.offset, 10, 2)
+        when o.offset = -999.0 then "       N/A"
+        when o.offset = -1.0 then "  syntenic"
+        end
+, o.symbol "Symbol", o.name "Name"
+from #output o
 go
 
