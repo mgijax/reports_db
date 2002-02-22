fdd6e94fb0d765ac29435c0733911b8fff921cd0
Lori Corbani <Lori.Corbani@jax.org>
2002-02-22 18:27:44 +0000

    remove external ids

--- daily/PRB_Strain.py
+++ daily/PRB_Strain.py
@@ -43,10 +43,6 @@ cmds = []
 cmds.append('select distinct a._Object_key, a.accID from PRB_Strain_Acc_View a ' + \
 	'where a._LogicalDB_key = 1 and a.prefixPart = "MGI:" and a.preferred = 1')
 
-# External Accession IDs
-
-cmds.append('select distinct a._Object_key, a.accID from PRB_Strain_Acc_View a where a._LogicalDB_key != 1')
-
 # Retrieve markers
 
 cmds.append('select _Strain_key, symbol from PRB_Strain_Marker_View')
@@ -62,7 +58,6 @@ cmds.append('select _Strain_key, strain from PRB_Strain where standard = 1 and p
 results = db.sql(cmds, 'auto')
 
 mgiIDs = {}
-externalIDs = {}
 markers = {}
 syns = {}
 
@@ -70,27 +65,20 @@ for r in results[0]:
 	mgiIDs[r['_Object_key']] = r['accID']
 	
 for r in results[1]:
-	if externalIDs.has_key(r['_Object_key']):
-		externalIDs[r['_Object_key']].append(r['accID'])
-	else:
-		externalIDs[r['_Object_key']] = []
-		externalIDs[r['_Object_key']].append(r['accID'])
-	
-for r in results[2]:
 	if markers.has_key(r['_Strain_key']):
 		markers[r['_Strain_key']].append(r['symbol'])
 	else:
 		markers[r['_Strain_key']] = []
 		markers[r['_Strain_key']].append(r['symbol'])
 
-for r in results[3]:
+for r in results[2]:
 	if syns.has_key(r['_Strain_key']):
 		syns[r['_Strain_key']].append(r['synonym'])
 	else:
 		syns[r['_Strain_key']] = []
 		syns[r['_Strain_key']].append(r['synonym'])
 
-for r in results[4]:
+for r in results[3]:
 	if mgiIDs.has_key(r['_Strain_key']):
 		fp.write(mgiIDs[r['_Strain_key']] + reportlib.TAB)
 	else:
@@ -100,11 +88,6 @@ for r in results[4]:
 
 	if markers.has_key(r['_Strain_key']):
 		fp.write(string.joinfields(markers[r['_Strain_key']], '|') + reportlib.TAB)
-	else:
-		fp.write(reportlib.TAB)
-
-	if externalIDs.has_key(r['_Strain_key']):
-		fp.write(string.joinfields(externalIDs[r['_Strain_key']], '|') + reportlib.TAB)
 	else:
 		fp.write(reportlib.TAB)
 
