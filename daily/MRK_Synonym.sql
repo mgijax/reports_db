717ee3b6dfc62ef65213f5c68e57f3d65908c003
Lori Corbani <Lori.Corbani@jax.org>
2002-03-21 16:29:43 +0000

    TR 2986

---
+++ daily/MRK_Synonym.sql
@@ -0,0 +1,21 @@
+print ""
+print "Marker Symbols and their Synonyms (ordered by Marker Symbol)"
+print ""
+
+select a.accID "MGI Accession ID", m.chromosome "Chr", "cM Position" =
+        case
+        when o.offset >= 0 then str(o.offset, 10, 2)
+        when o.offset = -999.0 then "       N/A"
+        when o.offset = -1.0 then "  syntenic"
+        end,
+"Marker Symbol" = m.symbol, "Synonym" = substring(s.name,1,90)
+from MRK_Marker m, MRK_Acc_View a, MRK_Other s, MRK_Offset o 
+where m._Species_key = 1 
+and m._Marker_key = a._Object_key 
+and a.prefixPart = "MGI:" 
+and a.preferred = 1 
+and m._Marker_key = o._Marker_key 
+and o.source = 0 
+and m._Marker_key = s._Marker_key
+order by m.symbol
+go
