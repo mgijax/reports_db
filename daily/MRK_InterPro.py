5e4b38bc66eb3c47e5ae0fe7649660e025bbafcb
Lori Corbani <Lori.Corbani@jax.org>
2002-12-31 17:26:51 +0000

    TR 4292

---
+++ daily/MRK_InterPro.py
@@ -0,0 +1,68 @@
+#!/usr/local/bin/python
+
+'''
+#
+# Report:
+#       Tab-delimited file
+#       Mouse Markers and their Interpro Acc Id's
+#
+# Usage:
+#       MRK_InterPro.py
+#
+#
+# Notes:
+#
+# History:
+#
+# lec	12/31/2002
+#	- made a regular nightly report
+#
+# pm	11/21/2001
+#
+'''
+ 
+import sys
+import os
+import db
+import mgi_utils
+import reportlib
+
+#
+# Main
+#
+
+fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
+
+# Retrieve MGI Accession number, InterPro accession Id's.
+
+cmd = 'select distinct mgiID = a1.accID, m.symbol, a2.accID ' + \
+      'from MRK_Marker m, MRK_Acc_View a1, MRK_Acc_View a2 ' + \
+      'where m._Species_key = 1 ' + \
+      'and m._Marker_key = a1._Object_key ' + \
+      'and a1._LogicalDB_key = 1 ' + \
+      'and a1.prefixPart = "MGI:" ' + \
+      'and a1.preferred = 1 ' + \
+      'and m._Marker_key = a2._Object_key ' + \
+      'and a2._LogicalDB_key = 28 ' + \
+      'order by m.symbol '
+
+results = db.sql(cmd, 'auto')
+
+prevMarker = ''
+
+for r in results:
+
+	if prevMarker == r['mgiID']:
+		fp.write(' ' + r['accID'])
+                prevMarker = r['mgiID']
+        else: 
+		if prevMarker != '':
+	        	fp.write(reportlib.CRT)
+		fp.write(r['mgiID'] + reportlib.TAB)
+		fp.write(r['symbol'] + reportlib.TAB)
+                fp.write(r['accID'])
+                prevMarker = r['mgiID']
+
+fp.write(reportlib.CRT)
+reportlib.finish_nonps(fp)
+
