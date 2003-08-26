afec03a96b9693ddd9b41a0f82fbfb956456627f
Lori Corbani <Lori.Corbani@jax.org>
2003-08-26 16:50:15 +0000

    TR 4970

---
+++ daily/MRK_ENSEMBL.py
@@ -0,0 +1,74 @@
+#!/usr/local/bin/python
+
+'''
+#
+# TR 4970 (custom SQL TR 4338)
+#
+# Report:
+#       Produce a tab-delimited report with the following output fields:
+#
+#       MGD ID
+#       Gene Symbol
+#       Gene Name
+#       Chromosome
+#       cM Position
+#       Ensembl Gene ID
+#
+# Usage:
+#       MRK_ENSEMBL.py
+#
+# Notes:
+#
+# History:
+#
+# lec	08/26/2003
+#	- added to reports_db product
+#	- changed order of cM position and chromosome to be consistent
+#	  with other Sequence reports.
+#	- added outputdir, import os
+#
+# dbm   12/13/2002
+#       - created
+#
+'''
+ 
+import sys 
+import os
+import db
+import reportlib
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+#
+# Main
+#
+
+fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
+
+cmd = 'select a1.accID "MGI", m.symbol, m.name, m.chromosome, o.offset, a2.accID "Ensembl" ' + \
+      'from ACC_Accession a1, ACC_Accession a2, MRK_Marker m, MRK_Offset o ' + \
+      'where a1._Object_key = a2._Object_key and ' + \
+            'a1._Object_key = m._Marker_key and ' + \
+            'm._Marker_key = o._Marker_key and ' + \
+            'a1._LogicalDB_key = 1 and ' + \
+            'a1._MGIType_key = 2 and ' + \
+            'a1.prefixPart = "MGI:" and ' + \
+            'a1.preferred = 1 and ' + \
+            'a2._LogicalDB_key = 33 and ' + \
+            'a2._MGIType_key = 2 and ' + \
+            'm._Marker_Type_key = 1 and ' + \
+            'o.source = 0 ' + \
+      'order by m.chromosome, m.symbol'
+
+results = db.sql(cmd, 'auto')
+
+for r in results:
+    fp.write(r['MGI'] + TAB + r['symbol'] + TAB + r['name'] + TAB +
+	     str(r['offset']) + TAB +
+             r['chromosome'] + TAB + 
+             r['Ensembl'] + CRT)
+
+reportlib.finish_nonps(fp)
