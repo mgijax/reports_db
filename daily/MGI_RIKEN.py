4c6409be08d4a93ba398128073b17f0224715a1a
Lori Corbani <Lori.Corbani@jax.org>
2003-10-27 20:15:20 +0000

    TR 5252

--- daily/MGI_RIKEN.py
+++ daily/MGI_RIKEN.py
@@ -59,6 +59,11 @@ cmds.append('select a1._Object_key, cloneID = a1.accID, mgiID = a2.accID ' + \
 
 cmds.append('create nonclustered index idx_key on #riken(_Object_key)')
 
+cmds.append('select r._Object_key, a.accID ' + \
+    'from #riken r, PRB_Acc_View a ' + \
+    'where r._Object_key = a._Object_key ' + \
+    'and a._LogicalDB_key = 9')
+
 # problem sequences
 cmds.append('select distinct r._Object_key ' + \
 	'from #riken r, PRB_Notes n ' + \
@@ -82,6 +87,12 @@ cmds.append('select r.*, markerID = ma.accID, m.symbol, m.name ' + \
 
 results = db.sql(cmds, 'auto')
 
+seqIDs = {}
+for r in results[-3]:
+    key = r['_Object_key']
+    value = r['accID']
+    seqIDs[key] = value
+
 problemClones = []
 for r in results[-2]:
     problemClones.append(r['_Object_key'])
@@ -94,8 +105,12 @@ for r in results[-1]:
 	    isProblem = ''
 
 	fp.write(r['cloneID'] + TAB + \
-	    r['mgiID'] + TAB + \
-	    isProblem + TAB + \
+	    r['mgiID'] + TAB)
+
+        if seqIDs.has_key(r['_Object_key']):
+	    fp.write(seqIDs[r['_Object_key']])
+
+        fp.write(TAB + isProblem + TAB + \
 	    mgi_utils.prvalue(r['markerID']) + TAB + \
 	    mgi_utils.prvalue(r['symbol']) + TAB + \
 	    mgi_utils.prvalue(r['name']) + CRT)
