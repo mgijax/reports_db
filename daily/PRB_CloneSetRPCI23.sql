34c257bef36037a13f340c4248700af62c84aec7
dow <>
2004-04-06 13:16:29 +0000

    The Clone Set of RPCI-23.

---
+++ daily/PRB_CloneSetRPCI23.sql
@@ -0,0 +1,40 @@
+
+set nocount on
+go
+
+select distinct p._Probe_key, pm._Marker_key
+into #prbs
+from PRB_Source ps, PRB_Probe p, MGI_Set s, MGI_SetMember sm, PRB_Marker pm
+where s._Set_key = sm._Set_key 
+and s._MGIType_key = 5
+and s.name = 'RPCI-23'
+and sm._Object_key = ps._Source_key
+and ps._Source_key = p._Source_key
+and p._Probe_key *= pm._Marker_key
+go
+
+set nocount off
+go
+
+print ""
+print "MGI Clone Set: RPCI-23"
+print ""
+print "Description: A row in this report represents a member of the Clone"
+print "Set:'RPCI-23'.  Note a clone in this report may also exist in one"
+print "of the other Clone Set reports."
+print ""
+
+select segmentID = pa.accID, segmentName = p.name, markerID = ma.accID, 
+markerSymbol = m.symbol
+from #prbs tmp, PRB_Probe p, PRB_ACC_View pa, MRK_Marker m, MRK_ACC_View ma
+where tmp._Probe_key = p._Probe_key 
+and p._Probe_key = pa._Object_key 
+and pa.preferred = 1
+and pa.prefixPart = 'MGI:'
+and tmp._Marker_key *= m._Marker_key 
+and tmp._Marker_key *= ma._Object_key
+and ma.preferred = 1
+and ma.prefixPart = 'MGI:'
+order by markerSymbol, pa.accID 
+go
+
