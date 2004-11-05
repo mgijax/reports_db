6c072ba00ae968bfd600cf48a3bfa620d12eda54
Lori Corbani <Lori.Corbani@jax.org>
2004-11-05 16:22:18 +0000

    performance tuning

--- daily/MRK_Dump1.py
+++ daily/MRK_Dump1.py
@@ -28,17 +28,34 @@ import os
 import db
 import reportlib
 
+db.useOneConnection(1)
+
 fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
 
-command = 'select symbol, mgiID ' + \
-	  'from MRK_Mouse_View ' + \
-	  'where _Marker_Status_key in (1,3) ' + \
-	  'order by symbol'
-results = db.sql(command, 'auto')
+cmds = []
+cmds.append('select _Marker_key, symbol ' + \
+	'into #markers ' + \
+	'from MRK_Marker ' + \
+	'where _Organism_key = 1 ' + \
+	'and _Marker_Status_key in (1,3)')
+cmds.append('create index idx1 on #markers(_Marker_key)')
+cmds.append('create index idx2 on #markers(symbol)')
+db.sql(cmds, None)
+
+results = db.sql('select m.symbol, a.accID ' + \
+	'from #markers m, ACC_Accession a ' + \
+	'where m._Marker_key = a._Object_key ' + \
+	'and a._MGIType_key = 2 ' + \
+	'and a._LogicalDB_key = 1 ' + \
+	'and a.prefixPart = "MGI:" ' + \
+	'and a.preferred = 1 ' + \
+	'order by m.symbol', 'auto')
 
 for r in results:
-	fp.write(r['mgiID'] + reportlib.TAB + \
+	fp.write(r['accID'] + reportlib.TAB + \
 	         r['symbol'] + reportlib.CRT)
 
 reportlib.finish_nonps(fp)
+
+db.useOneConnection(0)
 
--- daily/MRK_Dump2.py
+++ daily/MRK_Dump2.py
@@ -30,16 +30,41 @@ import os
 import db
 import reportlib
 
-fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
-
 command = 'select symbol, name, offset_str, chromosome, mgiID, markerType ' + \
 	  'from MRK_Mouse_View ' + \
 	  'where _Marker_Status_key in (1,3) ' + \
 	  'order by symbol'
-results = db.sql(command, 'auto')
+
+reportlib.finish_nonps(fp)
+
+db.useOneConnection(1)
+
+fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
+
+cmds = []
+cmds.append('select m._Marker_key, m.symbol, m.chromosome, markerType = t.term, offset_str = str(o.offset,10,2) ' + \
+	'into #markers ' + \
+	'from MRK_Marker m, VOC_Term t, MRK_Offset o ' + \
+	'where m._Organism_key = 1 ' + \
+	'and m._Marker_Status_key in (1,3) ' + \
+	'and m._Marker_Type_key = t._Term_key ' + \
+	'and m._Marker_key = o._Marker_key ' '+ \
+	'and o.source = 0')
+cmds.append('create index idx1 on #markers(_Marker_key)')
+cmds.append('create index idx2 on #markers(symbol)')
+db.sql(cmds, None)
+
+results = db.sql('select m.symbol, m.symbol, m.chromosome, m.markerType, m.offset_str, a.accID ' + \
+	'from #markers m, ACC_Accession a ' + \
+	'where m._Marker_key = a._Object_key ' + \
+	'and a._MGIType_key = 2 ' + \
+	'and a._LogicalDB_key = 1 ' + \
+	'and a.prefixPart = "MGI:" ' + \
+	'and a.preferred = 1 ' + \
+	'order by m.symbol', 'auto')
 
 for r in results:
-	fp.write(r['mgiID'] + reportlib.TAB + \
+	fp.write(r['accID'] + reportlib.TAB + \
 	         r['symbol'] + reportlib.TAB + \
 		 r['name'] + reportlib.TAB + \
 		 r['offset_str'] + reportlib.TAB + \
@@ -47,4 +72,6 @@ for r in results:
 		 r['markerType'] + reportlib.CRT)
 
 reportlib.finish_nonps(fp)
+
+db.useOneConnection(0)
 
