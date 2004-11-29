c385e928deff7241d8017f9fa51d6ff67ec45b43
Lori Corbani <Lori.Corbani@jax.org>
2004-11-29 17:36:57 +0000

    reports_db-22-0-7

--- daily/MRK_Sequence.py
+++ daily/MRK_Sequence.py
@@ -151,7 +151,7 @@ for r in results:
 	         r['name'] + reportlib.TAB + \
 	         offset + reportlib.TAB + \
 	         r['chromosome'] + reportlib.TAB + \
-		 string.join(gbID[key], ' ') + TAB)
+		 string.join(gbID[key], ' ') + reportlib.TAB)
 
 	if ugID.has_key(key):
 		fp.write(string.join(ugID[key], ' '))
