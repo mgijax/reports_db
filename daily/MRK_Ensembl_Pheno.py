b8296b8dcce0f459abda5d65b7cb1129648db9b7
Lori Corbani <Lori.Corbani@jax.org>
2005-03-22 19:07:19 +0000

    MPR

--- daily/MRK_Ensembl_Pheno.py
+++ daily/MRK_Ensembl_Pheno.py
@@ -53,49 +53,48 @@ PAGE = reportlib.PAGE
 
 fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
 
-cmds = []
+db.sql('select a1.accID "MGI", m.symbol, m.name, a2.accID "Ensembl", a._Annot_key, a._Term_key ' + \
+       'into #mgi_ensembl ' + \
+       'from MRK_Marker m, GXD_AlleleGenotype g, ACC_Accession a1, ACC_Accession a2, VOC_Annot a ' + \
+       'where m._Organism_key = 1 ' + \
+       'and m._Marker_Type_key = 1 ' + \
+       'and m._Marker_key = g._Marker_key ' + \
+       'and g._Genotype_key = a._Object_key ' + \
+       'and a._AnnotType_key = 1002 ' + \
+       'and m._Marker_key = a1._Object_key ' + \
+       'and a1._MGIType_key = 2 ' + \
+       'and a1._LogicalDB_key = 1 ' + \
+       'and a1.prefixPart = "MGI:" ' + \
+       'and a1.preferred = 1 ' + \
+       'and m._Marker_key = a2._Object_key ' + \
+       'and a2._MGIType_key = 2 ' + \
+       'and a2._LogicalDB_key = 33 ' + \
+       'and a2.preferred = 1', None)
 
-cmds.append(
-    'select a1.accID "MGI", m.symbol, m.name, ' + \
-           'a2.accID "Ensembl", a._Annot_key, a._Term_key ' + \
-    'into #mgi_ensembl ' + \
-    'from ACC_Accession a1, ACC_Accession a2, MRK_Marker m, ' + \
-         'GXD_AlleleGenotype g, VOC_Annot a ' + \
-    'where a1._Object_key = a2._Object_key and ' + \
-          'a1._Object_key = m._Marker_key and ' + \
-          'm._Marker_key = g._Marker_key and ' + \
-          'g._Genotype_key = a._Object_key and ' + \
-          'a1._LogicalDB_key = 1 and ' + \
-          'a1._MGIType_key = 2 and ' + \
-          'a1.prefixPart = "MGI:" and ' + \
-          'a1.preferred = 1 and ' + \
-          'a2._LogicalDB_key = 33 and ' + \
-          'a2._MGIType_key = 2 and ' + \
-          'a2.preferred = 1 and ' + \
-          'm._Marker_Type_key = 1 and ' + \
-          'a._AnnotType_key = 1002')
+db.sql('create index idx1 on #mgi_ensembl(_Term_key)', None)
+db.sql('create index idx2 on #mgi_ensembl(_Annot_key)', None)
+db.sql('create index idx3 on #mgi_ensembl(symbol)', None)
 
-cmds.append(
-    'select distinct m.MGI, m.symbol, m.name, m.Ensembl, ' + \
-           'a1.accID "classification", t.term, a2.accID "pubmed" ' + \
-    'from #mgi_ensembl m, VOC_Term t, VOC_Evidence e, ' + \
-         'ACC_Accession a1, ACC_Accession a2 ' + \
-    'where m._Term_key = t._Term_key and ' + \
-          't._Term_key = a1._Object_key and ' + \
-          'a1._MGIType_key = 13 and ' + \
-          'a1.preferred = 1 and ' + \
-          'm._Annot_key = e._Annot_key and ' + \
-          'e._Refs_key = a2._Object_key and ' + \
-          'a2._LogicalDB_key = 29 and ' + \
-          'a2.preferred = 1 ' + \
-    'order by m.symbol')
+results = db.sql('select distinct m.MGI, m.symbol, m.name, m.Ensembl, a1.accID "classification", t.term, a2.accID "pubmed" ' + \
+                 'from #mgi_ensembl m, VOC_Term t, VOC_Evidence e, ACC_Accession a1, ACC_Accession a2 ' + \
+                 'where m._Term_key = t._Term_key ' + \
+                 'and t._Term_key = a1._Object_key ' + \
+                 'and a1._MGIType_key = 13 ' + \
+                 'and a1.preferred = 1 ' + \
+                 'and m._Annot_key = e._Annot_key ' + \
+                 'and e._Refs_key = a2._Object_key ' + \
+                 'and a2._LogicalDB_key = 29 ' + \
+                 'and a2.preferred = 1 ' + \
+                 'order by m.symbol', 'auto')
 
-results = db.sql(cmds, 'auto')
-
-for r in results[1]:
-    fp.write(r['MGI'] + TAB + r['symbol'] + TAB + r['name'] + TAB +
-             r['Ensembl'] + TAB + r['classification'] + TAB +
-             r['term'] + TAB + r['pubmed'] + CRT)
+for r in results:
+    fp.write(r['MGI'] + TAB + \
+	     r['symbol'] + TAB + \
+	     r['name'] + TAB + \
+             r['Ensembl'] + TAB + \
+	     r['classification'] + TAB + \
+             r['term'] + TAB + \
+	     r['pubmed'] + CRT)
 
 reportlib.finish_nonps(fp)
 
