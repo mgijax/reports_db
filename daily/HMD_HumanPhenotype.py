aad803171228bf408b0c07e0dc010c49b1ccb79b
Lori Corbani <Lori.Corbani@jax.org>
2005-03-22 18:57:48 +0000

    MPR

--- daily/HMD_HumanPhenotype.py
+++ daily/HMD_HumanPhenotype.py
@@ -50,25 +50,6 @@ SPACE = reportlib.SPACE
 TAB = reportlib.TAB
 PAGE = reportlib.PAGE
 
-#############
-#           #
-# Functions #
-#           #
-#############
-'''
-# requires: results,    The array of data
-#           keyField,   The name of the key to use to 
-#                       create the dictionary entries
-#           valueField, The name of the row field to use
-#                       as the value for the dictionary entry
-# effects:
-#     Creates a dictionary from a result array from db.sql
-#     using the keyfield as the key and 
-#     the value field as the key value.
-#
-# returns:
-#     a dictionary
-'''
 def createDict(results, keyField, valueField):
 
         d = {}
@@ -80,22 +61,18 @@ def createDict(results, keyField, valueField):
                 d[key] = string.join([d[key],value,])
         return d
 
-########
-#      #
-# MAIN #
-#      #
-########
 
-db.useOneConnection(1)
+#
+# Main
+#
 
-#################################################################
-# Get mouse to human orthologous marker pair's symbols and keys #
-# and the Phenoslim IDs associated to the mouse markers and     #
-# place them in a temp table to be used by other queries        #
-# needed to produce the desired report                          #
-#################################################################
-cmds = []
-cmds.append('select distinct mouseKey = h1._Marker_key, mouseSym = m1.symbol, ' + \
+fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
+
+#
+# Get mouse to human orthologous marker pair's symbols and keys
+#
+
+db.sql('select distinct mouseKey = h1._Marker_key, mouseSym = m1.symbol, ' + \
 		'humanKey = h2._Marker_key, humanSym = m2.symbol ' + \
 		'into #homology ' + \
 		'from HMD_Homology r1, HMD_Homology_Marker h1, ' + \
@@ -107,20 +84,16 @@ cmds.append('select distinct mouseKey = h1._Marker_key, mouseSym = m1.symbol, ' 
 		'and r1._Class_key = r2._Class_key ' + \
 		'and r2._Homology_key = h2._Homology_key ' + \
 		'and h2._Marker_key = m2._Marker_key ' + \
-		'and m2._Organism_key = 2')
+		'and m2._Organism_key = 2', None)
 
-######################################################
-# Create some indexes to speed up the report queries #
-######################################################
-cmds.append('create nonclustered index index_mouseKey on #homology(mouseKey)')
-cmds.append('create nonclustered index index_humanKey on #homology(humanKey)')
-cmds.append('create nonclustered index index_humanSym on #homology(humanSym)')
-db.sql(cmds, None)
+db.sql('create nonclustered index index_mouseKey on #homology(mouseKey)', None)
+db.sql('create nonclustered index index_humanKey on #homology(humanKey)', None)
+db.sql('create nonclustered index index_humanSym on #homology(humanSym)', None)
 
-###################################################################
-# Get the MGI IDs for the Mouse markers in the temp table created #
-# by the first query                                              #
-###################################################################
+#
+# Get the MGI IDs for the mouse markers
+#
+
 results = db.sql('select a._Object_key, a.accID ' + \
 		'from #homology h, ACC_Accession a ' + \
 		'where a._Object_key = h.mouseKey ' + \
@@ -130,10 +103,10 @@ results = db.sql('select a._Object_key, a.accID ' + \
 		'and a.preferred = 1 ', 'auto')
 mmgi = createDict(results, '_Object_key', 'accID')
 
-#####################################################################
-# Get the EntrezGene for the Human markers in the temp table created # 
-# by the first query                                                #
-#####################################################################
+#
+# Get the EntrezGene for the human markers
+#
+
 results = db.sql('select a._Object_key, a.accID ' + \
                 'from #homology h, ACC_Accession a ' + \
                 'where a._Object_key = h.humanKey ' + \
@@ -141,10 +114,10 @@ results = db.sql('select a._Object_key, a.accID ' + \
                 'and a._LogicalDB_key = 55', 'auto')
 hlocus = createDict(results, '_Object_key', 'accID')
 
-#####################################################################
-# Get the Phenoslim for the Mouse markers in the temp table created # 
-# by the first query                                                #
-#####################################################################
+#
+# Get the MP for the mouse markers
+#
+
 results = db.sql('select distinct h.mouseKey, a.accID ' + \
                 'from #homology h, GXD_AlleleGenotype g, VOC_Annot v, ACC_Accession a ' + \
                 'where g._Marker_key = h.mouseKey ' + \
@@ -155,49 +128,21 @@ results = db.sql('select distinct h.mouseKey, a.accID ' + \
                 'and a._LogicalDB_key = 34 ', 'auto')
 mpheno = createDict(results, 'mouseKey', 'accID')
 
-#################################################################
-# Get the mouse to human orthologous marker pairs from the temp #
-# table created in the first query so that it can be used as    #
-# data for creating the desired report                          #
-#################################################################
 results = db.sql('select * from #homology order by humanSym', 'auto')
 
-####################
-# Open report file #
-####################
-fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = 0)
-
-############################
-# Process db query results #
-############################
 for r in results:
-    ################################
-    # Output the human gene symbol #
-    ################################
     fp.write(r['humanSym'] + TAB)
 
-    #####################################################
-    # If the human marker has a entrezgene ID output it #
-    #####################################################
     if hlocus.has_key(r['humanKey']):
         fp.write(hlocus[r['humanKey']] + TAB)
     else:
         fp.write(TAB)
 
-    ################################
-    # Output the mouse gene symbol #
-    ################################
     fp.write(r['mouseSym'] + TAB)
 
-    #########################################################
-    # If the mouse marker has an MGI accession ID output it #
-    #########################################################
     if mmgi.has_key(r['mouseKey']):
         fp.write(mmgi[r['mouseKey']] + TAB)
 
-    ##################################################
-    # The mouse marker has Phenoslim IDs output them #
-    ##################################################
     if mpheno.has_key(r['mouseKey']):
         fp.write(mpheno[r['mouseKey']] + TAB)
     else:
@@ -205,9 +150,4 @@ for r in results:
 
     fp.write(CRT)
 
-#########################
-# Close the output file #
-#########################
 reportlib.finish_nonps(fp)
-
-db.useOneConnection(0)
