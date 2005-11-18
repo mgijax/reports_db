e17c5fcd0a7101666761777cf7c0ec2c778e946e
Lori Corbani <Lori.Corbani@jax.org>
2005-11-18 15:46:27 +0000

    added clone ids

--- daily/PRB_CloneLibrarycDNA.py
+++ daily/PRB_CloneLibrarycDNA.py
@@ -41,11 +41,13 @@ TAB = reportlib.TAB
 
 fp = reportlib.init(sys.argv[0], fileExt = '.sql.rpt', title = 'MGI cDNA Clone Libraries', outputdir = os.environ['REPORTOUTPUTDIR'])
 fp.write(string.ljust('library name', 95))
-fp.write('clone collection' + CRT)
+fp.write(string.ljust('clone collection', 50))
+fp.write('clone ids' + CRT)
 fp.write(string.ljust('------------', 95))
-fp.write('----------------' + 2*CRT)
+fp.write(string.ljust('----------------', 50))
+fp.write('----------------' + CRT)
 
-db.sql('select name = substring(ps.name, 1, 90), cloneCollection = s.name ' + \
+db.sql('select _Source_key, name = substring(ps.name, 1, 90), cloneCollection = s.name ' + \
     'into #libraries ' + \
     'from MGI_Set s, MGI_SetMember sm, PRB_Source ps ' + \
     'where s._MGIType_key = 5 ' + \
@@ -53,7 +55,22 @@ db.sql('select name = substring(ps.name, 1, 90), cloneCollection = s.name ' + \
     'and s._Set_key = sm._Set_key ' + \
     'and sm._Object_key = ps._Source_key ', None)
 
-results = db.sql('select name, cloneCollection from #libraries order by name', 'auto') 
+accIDs = {}
+results = db.sql('select distinct l._Source_key, a.accID, ll.name from #libraries l, ACC_Accession a, ACC_LogicalDB ll ' + \
+	'where l._Source_key = a._Object_key ' + \
+	'and a._MGIType_key = 5 ' + \
+	'and a._LogicalDB_key = ll._LogicalDB_key', 'auto')
+for r in results:
+    key = r['_Source_key']
+    tokens = string.split(r['name'], ' ')
+    value = tokens[0] + ':' + string.strip(r['accID'])
+
+    if not accIDs.has_key(key):
+	accIDs[key] = []
+
+    accIDs[key].append(value)
+
+results = db.sql('select distinct _Source_key, name, cloneCollection from #libraries order by name', 'auto') 
 
 cloneLib = {}
 for r in results:
@@ -64,10 +81,16 @@ for r in results:
 	cloneLib[library] = []
     cloneLib[library].append(collection)
 
-results = db.sql('select distinct name from #libraries order by name', 'auto')
+results = db.sql('select distinct _Source_key, name from #libraries order by name', 'auto')
 for r in results:
     fp.write(string.ljust(r['name'], 95))
-    fp.write(string.join(cloneLib[r['name']], ',') + CRT)
+    fp.write(string.ljust(string.join(cloneLib[r['name']], ','), 50))
 
+    if accIDs.has_key(r['_Source_key']):
+        fp.write(string.ljust(string.join(accIDs[r['_Source_key']], ','), 50))
+
+    fp.write(CRT)
+
+fp.write(CRT + '(%d rows affected)' % (len(results)) + CRT)
 reportlib.trailer(fp)
 reportlib.finish_nonps(fp)
