f7671f88c599324e4e4a037f2532145620e4fbf3
Lori Corbani <Lori.Corbani@jax.org>
2005-12-06 18:11:25 +0000

    fix

--- daily/MGI_PhenotypicAllele.py
+++ daily/MGI_PhenotypicAllele.py
@@ -113,7 +113,8 @@ results = db.sql('select s._Marker_key, a.accID ' + \
 	'from #alleles s, ACC_Accession a ' + \
 	'where s._Marker_key = a._Object_key ' + \
 	'and a._MGIType_key = 2 ' + \
-	'and a._LogicalDB_key = 27 ', 'auto')
+	'and a._LogicalDB_key = 27 ' + \
+	'and a.prefixPart in ["NM_", "XM_"]', 'auto')
 refIDs = {}
 for r in results:
 	refIDs[r['_Marker_key']] = r['accID']
