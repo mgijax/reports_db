480ade76ff964af99258e8adbe882a72093b4031
Lori Corbani <Lori.Corbani@jax.org>
2006-10-25 16:17:52 +0000

    remove goa

--- nightly_reports.sh
+++ nightly_reports.sh
@@ -16,9 +16,6 @@ setenv LOG	${REPORTLOGSDIR}/`basename $0`.log
 rm -rf ${LOG}
 touch ${LOG}
 
-# GOA processing
-./goa.sh
-
 foreach i (daily/*.sql)
 echo $i, `date`
 reportisql.csh $i ${REPORTOUTPUTDIR}/`basename $i`.rpt ${MGD_DBSERVER} ${MGD_DBNAME} MGI >> ${LOG}
