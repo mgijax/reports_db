a1803a0efba3790886c1557c40f1c1a2e44212b2
David Miers <dave.miers@jax.org>
2008-01-07 18:19:56 +0000

    TR 8697

--- daily/GO_gp2protein.py
+++ daily/GO_gp2protein.py
@@ -14,11 +14,20 @@
 #
 #	A tab-delimited file in this format:
 #	field 1: MGI Marker ID
-#	field 2: SWP:#####;SWP:#####;...
+#       field 2: Swiss-Prot sequence ('SWP:' + seqID) OR
+#                TrEMBL sequence ('TR:' + seqID) OR
+#                RefSeq sequence ('NCBI:' + seqID) OR
+#
 # Used by:
 #
 # History:
-#	Created for TR3659.  
+#
+# 12/24/2007    dbm
+#       - TR 8697; complete re-write; Show all protein coding genes and
+#         their representative protein sequences, regardless of annotations.
+#
+# 6/6/2003    lec
+#	- Created for TR3659.  
 #	Report the MGI ID, SwissProt ID(s), and GO ID(s) for all mouse
 #	markers that have associated SwissProt sequences.
 
@@ -39,43 +48,65 @@ CRT = reportlib.CRT
 
 fp = reportlib.init('gp2protein', fileExt = '.mgi', outputdir = os.environ['REPORTOUTPUTDIR'], printHeading = None)
 
-# Retrieve Markers with GO Annotations that have SP IDs
-
 cmds = []
 
-cmds.append('select distinct m._Marker_key, a.accID, spID = a2.accID ' + \
-	'into #markers ' + \
-	'from MRK_Marker m, ACC_Accession a, ACC_Accession a2 ' + \
-	'where m._Marker_key = a._Object_key ' + \
-	'and a._MGIType_key = 2 ' + \
-	'and a._LogicalDB_key = 1 ' + \
-	'and a.prefixPart = "MGI:" ' + \
-	'and a.preferred = 1 ' + \
-	'and m._Marker_key = a2._Object_key ' + \
-	'and a2._MGIType_key = 2 ' + \
-	'and a2._LogicalDB_key = 13 ' + \
-	'and exists (select 1 from VOC_Annot v ' + \
-	'where v._AnnotType_key = 1000 ' + \
-	'and m._Marker_key = v._Object_key) ' + \
-	'order by a.accID')
+#
+# Get the representative sequence (_Qualifier_key = 615421) for each marker
+# if the sequence is:
+# Swiss-Prot (logical DB = 13)
+# TrEMBL (logical DB = 41
+# RefSeq (logical DB = 27 and accID starts with 'NP_' or 'XP_'
+#
+cmds.append('select mc._Marker_key, ' + \
+                   'mc.accID, ' + \
+                   'mc._LogicalDB_key ' + \
+            'into #markerseq ' + \
+            'from ACC_Accession a, SEQ_Marker_Cache mc ' + \
+            'where a._MGIType_key = 2 and ' + \
+                  '(a._LogicalDB_key in (13,41) or ' + \
+                  '(a._LogicalDB_key = 27 and ' + \
+                   'a.prefixPart in ("NP_","XP_"))) and ' + \
+                  'a._Object_key = mc._Marker_key and ' + \
+                  'a.accID = mc.accID and ' + \
+                  'mc._Qualifier_key = 615421 and ' + \
+                  'mc._Organism_key = 1')
 
-cmds.append('select _Marker_key, spID from #markers')
-cmds.append('select distinct _Marker_key, accID from #markers')
+#
+# Get the MGI ID for each of the markers.
+#
+cmds.append('select a.accID "mgiID", ' + \
+                   'ms.accID "seqID", ' + \
+                   'ms._LogicalDB_key ' + \
+            'from ACC_Accession a, #markerseq ms ' + \
+            'where a._Object_key = ms._Marker_key and ' + \
+                  'a._MGIType_key = 2 and ' + \
+                  'a._LogicalDB_key = 1 and ' + \
+                  'a.prefixPart = "MGI:" and ' + \
+                  'a.preferred = 1 ' + \
+            'order by a.accID')
 
+#
+# Get the results set.
+#
 results = db.sql(cmds, 'auto')
 
-spIDs = {}
-for r in results[-2]:
-    key = r['_Marker_key']
-    value = 'SWP:' + r['spID']
-    if not spIDs.has_key(key):
-	spIDs[key] = []
-    spIDs[key].append(value)
+#
+# Write a record to the report for each marker/sequence in the results set.
+#
+for r in results[1]:
+    mgiID = r['mgiID']
+    logicalDB = r['_LogicalDB_key']
 
-for r in results[-1]:
+    #
+    # Apply the proper prefix to the seq ID based on the logical DB.
+    #
+    if logicalDB == 13:
+        seqID = 'SWP:' + r['seqID']
+    elif logicalDB == 41:
+        seqID = 'TR:' + r['seqID']
+    else:
+        seqID = 'NCBI:' + r['seqID']
 
-    fp.write(r['accID'] + reportlib.TAB + \
-	string.join(spIDs[r['_Marker_key']], ';') + CRT)
+    fp.write(mgiID + TAB + seqID + CRT)
 
 reportlib.finish_nonps(fp)
-
