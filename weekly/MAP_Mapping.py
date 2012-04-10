1ef399be411ecc131b920ab40584d18d4e43e05a
Lori Corbani <Lori.Corbani@jax.org>
2012-04-10 17:14:10 +0000

    postgres options

--- weekly/MAP_Mapping.py
+++ weekly/MAP_Mapping.py
@@ -51,42 +51,6 @@ db.useOneConnection(1)
 allPanels = ['1839', '8', '7', '2944', '3241', '4867', '4868', '4377', '4378', '2941', '3383', '4347', '6', '4869']
 allChromosomes = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','X','Y','XY']
 
-cmd = []
-
-cmd.append('''select _Cross_key, rowNumber, colNumber, data 
-			from CRS_Typings
-			order by _Cross_key, rowNumber, colNumber''')
-
-cmd.append('''select _Cross_key, whoseCross, femaleStrain, maleStrain,
-			convert(varchar, modification_date, 106) as modification_date, abbrevHT, abbrevHO, strainHT, strainHO 
-			from crs_cross_view
-			where _Cross_key in (1839, 8, 7, 2944, 3241, 4867, 4868, 4377, 4378, 2941, 3383, 4347, 6, 4869)''')
-
-cmd.append('''select _Cross_key, sequenceNum, name 
-			from CRS_Progeny
-			order by _Cross_key, sequenceNum''')
-			
-cmd.append('''select bv.jnumID, cr._Cross_key, cr._Marker_key from CRS_References cr, BIB_View bv
-			where cr._Refs_key != null
-			and cr._Refs_key = bv._Refs_key
-			order by cr._Cross_key, cr._Marker_key, bv.jnumID''')
-
-cmd.append('''select distinct cm._Marker_key, a.accID 
-			into #tmp_accID
-			from CRS_Matrix cm LEFT OUTER JOIN ACC_Accession a on (
-			cm._Marker_key = a._Object_key and a._MGIType_key = 2 and a.prefixPart = 'MGI:'
-			and a.private != 1 and a.preferred = 1)''')
-
-cmd.append('''select mc._Marker_key, a.accID
-			from MRK_Current mc, ACC_Accession a
-			where mc._Marker_key in (select _Marker_key from #tmp_accID where accID = null)
-			and mc._Current_key = a._Object_key and a.prefixPart = 'MGI:'
-			and a.private != 1 and a.preferred = 1 and a._MGIType_key = 2''')
-			
-results = db.sql(cmd, 'auto')
-
-# This report requires a ton of hashmaps that are used to print out certain formatted columns in the report
-
 typings = {}
 typings2 = {}
 cross = {}
@@ -104,7 +68,10 @@ preheader = {}
 preheader2 = {}
 mergedID = {}
 
-for item in results[0]:
+results = db.sql('''select _Cross_key, rowNumber, colNumber, data 
+			from CRS_Typings
+			order by _Cross_key, rowNumber, colNumber''', 'auto')
+for item in results:
 	key = 'cross_key' + str(item['_Cross_key']) + 'rowNumber' + str(item['rowNumber'])
 	itemList = item['data'].rstrip().split(' ')
 	
@@ -131,12 +98,12 @@ for key in typings:
 	preheader2[crsKey] = 'CHR'.ljust(8) + 'MGI ID'.ljust(12) + 'SYMBOL'.ljust(20) + 'ANIMAL#'.ljust(11) + ' '*6*(len(temp3)-2) + ' J:NUMBER'
 	preheader2[crsKey] = preheader2[crsKey] + '\n' + '-'*7 + ' ' + '-'*11 + ' ' + '-'*19 + ' '*6*len(temp3)+ ' ' + '-'*16
 	
-	
 
-
-	
-
-for item in results[1]:
+results = db.sql('''select _Cross_key, whoseCross, femaleStrain, maleStrain,
+			convert(char(20), modification_date, 100) as modification_date, abbrevHT, abbrevHO, strainHT, strainHO 
+			from crs_cross_view
+			where _Cross_key in (1839, 8, 7, 2944, 3241, 4867, 4868, 4377, 4378, 2941, 3383, 4347, 6, 4869)''', 'auto')
+for item in results:
 	key = str(item['_Cross_key'])
 	cross[key] = item['whoseCross']
 	
@@ -145,13 +112,16 @@ for item in results[1]:
 	else:
 		strain[key] = item['femaleStrain']
 		
-	lastMod[key] = item['modification_date'].replace(" ", "-")
+	lastMod[key] = str(item['modification_date'])
 	abbrevHT[key] = item['abbrevHT']
 	abbrevHO[key] = item['abbrevHO']
 	strainHT[key] = item['strainHT']
 	strainHO[key] = item['strainHO']
-	
-for item in results[2]:
+
+results = db.sql('''select _Cross_key, sequenceNum, name 
+			from CRS_Progeny
+			order by _Cross_key, sequenceNum''', 'auto')
+for item in results:
 	key = 'cross_key' + str(item['_Cross_key'])
 	if key in header:
 		header[key] = header[key] + item['name'] + TAB
@@ -160,14 +130,30 @@ for item in results[2]:
 		header[key] = TAB + TAB + TAB + item['name'] + TAB
 		header2[key] = ' '*40 + str(item['name']) + ' '*(6-len(item['name']))	
 
-for item in results[3]:
+results = db.sql('''select bv.jnumID, cr._Cross_key, cr._Marker_key from CRS_References cr, BIB_View bv
+			where cr._Refs_key != null
+			and cr._Refs_key = bv._Refs_key
+			order by cr._Cross_key, cr._Marker_key, bv.jnumID''', 'auto')
+for item in results:
 	key = 'cross_key' + str(item['_Cross_key']) + 'marker_key' + str(item['_Marker_key'])
 	if key in refs: 
 		refs[key] = refs[key] + ', ' + item['jnumID']
 	else:
 		refs[key] = item['jnumID']
 
-for item in results[5]:
+
+db.sql('''select distinct cm._Marker_key, a.accID 
+			into #tmp_accID
+			from CRS_Matrix cm LEFT OUTER JOIN ACC_Accession a on (
+			cm._Marker_key = a._Object_key and a._MGIType_key = 2 and a.prefixPart = 'MGI:'
+			and a.private = 0 and a.preferred = 1)''', None)
+
+results = db.sql('''select mc._Marker_key, a.accID
+			from MRK_Current mc, ACC_Accession a
+			where mc._Marker_key in (select _Marker_key from #tmp_accID where accID = null)
+			and mc._Current_key = a._Object_key and a.prefixPart = 'MGI:'
+			and a.private = 0 and a.preferred = 1 and a._MGIType_key = 2''', 'auto')
+for item in results:
 	mergedID[item['_Marker_key']] = item['accID']
 
 cmd = '''select distinct cm.chromosome, cm._Marker_key, cm._Cross_key, cm.rowNumber, 
@@ -175,7 +161,7 @@ cmd = '''select distinct cm.chromosome, cm._Marker_key, cm._Cross_key, cm.rowNum
 		from CRS_Matrix cm
 		     LEFT OUTER JOIN MRK_Marker mm on (cm._Marker_key = mm._Marker_key)
 		     LEFT OUTER JOIN ACC_Accession a on (cm._Marker_key = a._Object_key
-		         and a.prefixPart = 'MGI:' and a.private != 1 and a.preferred = 1 and a._MGIType_key = 2)
+		         and a.prefixPart = 'MGI:' and a.private = 0 and a.preferred = 1 and a._MGIType_key = 2)
 		     LEFT OUTER JOIN CRS_References cr on (cm._Cross_key = cr._Cross_key
 		         and cm._Marker_key = cr._Marker_key)
 		where cm._Cross_key = %s
@@ -234,7 +220,7 @@ for panel in allPanels:
 					line = line + TAB + accID
 					line2 = line2 + accID.ljust(12)
 			else:
-				if marker_status == 2:
+				if marker_status == 2 and mergedID.has_key(item['_Marker_key']):
 					line = line + TAB + mergedID[item['_Marker_key']]
 					line2 = line2 + mergedID[item['_Marker_key']].ljust(12)					
 				else:							
--- weekly/MGI_AllGenes.py
+++ weekly/MGI_AllGenes.py
@@ -343,7 +343,7 @@ def process():
     #  VEGA
     #
 
-    db.sql('select m._Marker_key, m.symbol, name = substring(m.name,1,75), ma.accID ' + \
+    db.sql('select m._Marker_key, m.symbol, substring(m.name,1,75) as name, ma.accID ' + \
 	    'into #markers ' + \
 	    'from MRK_Marker m, ACC_Accession ma ' + \
 	    'where m._Organism_key = 1 ' + \
@@ -364,8 +364,8 @@ def process():
     #
 
     db.sql('select m.*, l.chromosome, l.strand, c.sequenceNum, ' + \
-	    'startC = convert(int, l.startCoordinate), ' + \
-	    'endC = convert(int, l.endCoordinate) ' + \
+	    'convert(int, l.startCoordinate) as startC, ' + \
+	    'convert(int, l.endCoordinate) as endC ' + \
 	    'into #location ' + \
 	    'from #markers m, MRK_Location_Cache l, MRK_Chromosome c ' + \
 	    'where m._Marker_key = l._Marker_key ' + \
