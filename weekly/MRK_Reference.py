1a3f091d9772e1e40732d8a66020f598f1fe6183
Lori Corbani <Lori.Corbani@jax.org>
2012-04-11 12:35:37 +0000

    postgres options

--- weekly/MRK_Reference.py
+++ weekly/MRK_Reference.py
@@ -54,34 +54,40 @@ fp = reportlib.init(sys.argv[0], outputdir = os.environ['REPORTOUTPUTDIR'], prin
 #
 # mouse markers
 #
-db.sql('select a.accID, m._Marker_key, m.symbol, m.name ' + \
-	'into #markers ' + \
-	'from MRK_Marker m, ACC_Accession a ' + \
-	'where m._Organism_key = 1 ' + \
-	'and m._Marker_Status_key in (1,3) ' + \
-	'and m._Marker_key = a._Object_key ' + \
-	'and a._MGIType_key = 2 ' + \
-	'and a.prefixPart = "MGI:" ' + \
-	'and a.preferred = 1', None)
+db.sql('''
+	select a.accID, m._Marker_key, m.symbol, m.name 
+	into #markers 
+	from MRK_Marker m, ACC_Accession a 
+	where m._Organism_key = 1 
+	and m._Marker_Status_key in (1,3) 
+	and m._Marker_key = a._Object_key 
+	and a._MGIType_key = 2 
+	and a.prefixPart = "MGI:" 
+	and a.preferred = 1
+	''', None)
 db.sql('create index idx_marker on #markers(_Marker_key)', None)
 
 #
-# references
+# refs
 #
-db.sql('select distinct m._Marker_key, r._Refs_key ' + \
-	'into #references ' + \
-	'from #markers m, MRK_Reference r ' + \
-	'where m._Marker_key = r._Marker_key', None)
-db.sql('create index idx_refs on #references(_Refs_key)', None)
+db.sql('''
+	select distinct m._Marker_key, r._Refs_key 
+	into #refs 
+	from #markers m, MRK_Reference r 
+	where m._Marker_key = r._Marker_key
+	''', None)
+db.sql('create index idx_refs on #refs(_Refs_key)', None)
 
 #
 # pub med ids
 #
-results = db.sql('select r._Marker_key, a.accID ' + \
-	'from #references r, ACC_Accession a ' + \
-	'where a._MGIType_key = 1 ' + \
-	'and r._Refs_key = a._Object_key ' + \
-	'and a._LogicalDB_key = 29', 'auto')
+results = db.sql('''
+	select r._Marker_key, a.accID 
+	from #refs r, ACC_Accession a 
+	where a._MGIType_key = 1 
+	and r._Refs_key = a._Object_key 
+	and a._LogicalDB_key = 29
+	''', 'auto')
 pubmed = {}
 for r in results:
 	key = r['_Marker_key']
@@ -92,12 +98,14 @@ for r in results:
 #
 # synonyms
 #
-results = db.sql('select m._Marker_key, s.synonym ' + \
-	'from #markers m, MGI_Synonym s, MGI_SynonymType st ' + \
-	'where m._Marker_key = s._Object_key ' + \
-	'and s._MGIType_key = 2 ' + \
-	'and s._SynonymType_key = st._SynonymType_key ' + \
-	'and st.synonymType = "exact"', 'auto')
+results = db.sql('''
+	select m._Marker_key, s.synonym 
+	from #markers m, MGI_Synonym s, MGI_SynonymType st 
+	where m._Marker_key = s._Object_key 
+	and s._MGIType_key = 2 
+	and s._SynonymType_key = st._SynonymType_key 
+	and st.synonymType = 'exact'
+	''', 'auto')
 syn = {}
 for r in results:
 	key = r['_Marker_key']
