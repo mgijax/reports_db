4fccff18ee18033dc68de921fe14bcdeb820f99c
Lori Corbani <Lori.Corbani@jax.org>
2012-04-26 13:48:14 +0000

    reports_db-5-0-0-6

--- weekly/ALL_CellLine_GeneTrap.py
+++ weekly/ALL_CellLine_GeneTrap.py
@@ -12,20 +12,30 @@
 # Used by:
 #	NCBI Deanna Church
 #
+#
+# Gene Trap Alleles, 
+# 	. markers
+#	. mutant cell line data (sequence)
+#	. cell line information (vector, strain, etc.)
+#	. imsr counts of alleles by es cell line
+#	. imsr counts of markers by es cellline
+#	. imsr counts of alleles by strain
+#	. imsr counts of markers by strain
+#
 # Output Format:
 #
-#   1. mclID
-#   2. vector
-#   3. mclCreator
-#   4. mclLibrary - derivationName
-#   5. parentCellLine
-#   6. parentCellLineStrain
-#   7. alleleID
-#   8. alleleSymbol
-#   9. alleleName
+#   1.  mclID
+#   2.  vector
+#   3.  mclCreator
+#   4.  mclLibrary - derivationName
+#   5.  parentCellLine
+#   6.  parentCellLineStrain
+#   7.  alleleID
+#   8.  alleleSymbol
+#   9.  alleleName
 #   10. alleleType
 #   11. markerID
-#   12. markerSymbol
+#   12. markersymbol
 #   13. esIMSR - list of IMSR providers holding es cell lines 
 #	associated to the Gene/Allele
 #   14. strainIMSR - list of IMSR providers holding strain stock 
@@ -33,6 +43,10 @@
 #   15. id.version
 #   16. tag = Tag Method
 #
+# 04/26/2012	lec
+#	- TR11035/re-organized this report to begin from the perspective of the Gene Trap Alleles
+#	- the previous version took 25-30 minutes to run 8 minutes
+#	
 # 04/09/2012	lec
 #	- convert to using one db.sql() per sql command in preparation for postgres
 #
@@ -62,7 +76,6 @@ try:
 except:
     import db
 
-
 CRT = reportlib.CRT
 TAB = reportlib.TAB
 
@@ -88,111 +101,139 @@ fp.write('strainIMSR' + TAB)
 fp.write('id.version' + TAB)
 fp.write('tag' + CRT)
 
+#
+# gene trap alleles
+#
 db.sql('''
-	select distinct s._Object_key, s.accID, ss.version, 
-	    ac.accID as alleleID, 
-	    a.symbol, a.name, m.accID as markerID, 
-	    a.markerSymbol as markerSymbol,
-	    allt.term as alleleType, a._Allele_key, t.term 
-	into #gt_seqs 
-	from SEQ_Summary_View s, SEQ_Sequence ss, SEQ_GeneTrap sg, VOC_Term t, 
-	    SEQ_Allele_Assoc sa
-                LEFT OUTER JOIN ALL_Allele_View a on (
-                            sa._Allele_key = a._Allele_key
-                            and a._Allele_Status_key in (847114, 3983021))
-                LEFT OUTER JOIN ACC_Accession ac on (
-                            a._Allele_key = ac._Object_key
-	                    and ac._LogicalDB_key = 1 
-	                    and ac._MGIType_key = 11 
-	                    and ac.private = 0 
-	                    and ac.preferred = 1
-                            )
-                LEFT OUTER JOIN VOC_Term allt on (a._Allele_Type_key = allt._Term_key)
-                LEFT OUTER JOIN ACC_Accession m on (
-                            a._Marker_key = m._Object_key
-                            and m._LogicalDB_key = 1
-                            and m._MGIType_key = 2
-                            and m.private = 0
-                            and m.preferred = 1
-                            )
-	where s._LogicalDB_key = 9  
-	    and s._MGIType_key = 19
-	    and s._Object_key = ss._Sequence_key 
-	    and s._Object_key = sg._Sequence_key 
-	    and sg._TagMethod_key = t._Term_key 
-	    and s._Object_key = sa._Sequence_key
-	    ''', None)
+	select a._Allele_key, 
+	       a.symbol, 
+	       a.name,
+	       t1.term as alleleType,
+	       ac.accID as alleleID
+	into #genetrap
+	from ALL_Allele a, VOC_Term t1, ACC_Accession ac
+	where a._Allele_Status_key in (847114, 3983021)
+	and a._Allele_Status_key = t1._Term_key
+	and a._Allele_Type_key = 847121
+        and a._Allele_key = ac._Object_key
+        and ac._LogicalDB_key = 1 
+        and ac._MGIType_key = 11 
+        and ac.preferred = 1
+	''', None)
+db.sql('create index genetrap_idx1 on #genetrap(_Allele_key)', None)
+db.sql('create index genetrap_idx2 on #genetrap(alleleID)', None)
 
-db.sql('create index idx_gtallelekey on #gt_seqs (_Allele_key)', None)
-db.sql('create index idx_gtallele on #gt_seqs (alleleID)', None)
-db.sql('create index idx_gtmarker on #gt_seqs (markerID)', None)
-
+#
+# gene trap/markers
+#
 db.sql('''
-	select g._Allele_key, cv.vector, cv.parentCellLine, s2.strain 
-	into #cells
-	from #gt_seqs g, ALL_Allele_CellLine_View cv, ALL_CellLine p, 
-	    PRB_Strain s2 
-	where g._Allele_key = cv._Allele_key  
-	    and cv.isMutant = 1  
-	    and cv.parentCellLine_key = p._CellLine_key 
-	    and p._Strain_key = s2._Strain_key
-	    ''', None)
-db.sql('create index idx_alleleKey on #cells (_Allele_key)', None)
+	select a._Allele_key, m.symbol as markersymbol, ma.accID as markerID
+	into #markers
+	from #genetrap a, ALL_Marker_Assoc ama, MRK_Marker m, ACC_Accession ma
+	where a._Allele_key = ama._Allele_key
+	and ama._Marker_key = m._Marker_key
+        and m._Marker_key = ma._Object_key
+        and ma._LogicalDB_key = 1
+        and ma._MGIType_key = 2
+        and ma.preferred = 1
+	''', 'auto')
+db.sql('create index markers_idx1 on #markers(_Allele_key)', None)
+db.sql('create index markers_idx2 on #markers(markerID)', None)
+
+#
+# gene trap sequences
+#
+db.sql('''
+	select g._Allele_key, s.accID, ss.version, t.term as tag
+	into #sequences 
+	from #genetrap g, 
+	     SEQ_Summary_View s, 
+	     SEQ_Sequence ss, 
+	     SEQ_GeneTrap sg, 
+	     SEQ_Allele_Assoc sa, 
+	     VOC_Term t
+	where g._Allele_key = sa._Allele_key
+	and sa._Sequence_key = s._Object_key
+	and s._LogicalDB_key = 9  
+	and s._MGIType_key = 19
+	and s._Object_key = ss._Sequence_key 
+	and s._Object_key = sg._Sequence_key 
+	and sg._TagMethod_key = t._Term_key 
+	''', None)
+db.sql('create index sequences_idx1 on #sequences (_Allele_key)', None)
+
+#
+# cell line info
+#
+db.sql('''
+	select g._Allele_key, 
+	       cv.cellLine as mclID,
+	       cv.cellLineStrain as parentCellLineStrain,
+	       cv.creator as mclCreator,
+	       cv.vector, 
+	       cv.parentCellLine, 
+	       cv.derivationName as mclLibrary
+	into #celllines
+	from #genetrap g, ALL_Allele_CellLine_View cv
+	where g._Allele_key = cv._Allele_key and cv.isMutant = 1  
+	''', None)
+db.sql('create index celllines_idx1 on #celllines (_Allele_key)', None)
+
+#
+# imsr counts
+#
 db.sql('create table #imsrCounts(accID varchar(30), abbrevName varchar(30), cType int)', None)
-	
+
+# counts by allele type 1 (es cell line)
 db.sql('''
 	insert into #imsrCounts 
 	select distinct ac.accID, f.abbrevName, 1
-	from #gt_seqs g, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga, 
-	    imsr..Accession ac, imsr..Facility f 
-	where ac.accID = g.alleleID 
+	from #genetrap g, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga, imsr..Accession ac, imsr..Facility f 
+	where g.alleleID = ac.accID
 	and ac._IMSRType_key = 3 
 	and ac._Object_key = sga._Allele_key 
 	and sga._Strain_key = sfa._Strain_key
 	and sfa._StrainState_key = 2 
 	and sfa._Facility_key = f._Facility_key''', None)
 
+# counts by marker type 1 (es cell line)
 db.sql('''
 	insert into #imsrCounts 
 	select distinct ac.accID, f.abbrevName, 1 
-	from #gt_seqs g, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga, 
-	    imsr..Accession ac, imsr..Facility f 
-	where ac.accID = g.markerID 
+	from #markers m, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga, imsr..Accession ac, imsr..Facility f 
+	where m.markerID = ac.accID
 	and ac._IMSRType_key = 2 
 	and ac._Object_key = sga._Gene_key 
 	and sga._Strain_key = sfa._Strain_key
 	and sfa._StrainState_key = 2
 	and sfa._Facility_key = f._Facility_key''', None)
 
+# counts by allele type 2 (strain)
 db.sql('''
 	insert into #imsrCounts 
 	select distinct ac.accID, f.abbrevName, 2
-	from #gt_seqs g, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga,  
-	    imsr..Accession ac, imsr..Facility f 
-	where ac.accID = g.alleleID 
+	from #genetrap g, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga,  imsr..Accession ac, imsr..Facility f 
+	where g.alleleID = ac.accID
 	and ac._IMSRType_key = 3 
 	and ac._Object_key = sga._Allele_key 
 	and sga._Strain_key = sfa._Strain_key
 	and sfa._StrainState_key <> 2 
 	and sfa._Facility_key = f._Facility_key''', None)
 
+# counts by marker type 2 (strain)
 db.sql('''
 	insert into #imsrCounts 
 	select distinct ac.accID, f.abbrevName, 2
-	from #gt_seqs g, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga,
-	    imsr..Accession ac, imsr..Facility f 
-	where ac.accID = g.markerID 
-	    and ac._IMSRType_key = 2 
-	    and ac._Object_key = sga._Gene_key 
-	    and sga._Strain_key = sfa._Strain_key 
-	    and sfa._StrainState_key <> 2 
-	    and sfa._Facility_key = f._Facility_key''', None)
+	from #markers m, imsr..StrainFacilityAssoc sfa, imsr..SGAAssoc sga, imsr..Accession ac, imsr..Facility f 
+	where m.markerID = ac.accID
+	and ac._IMSRType_key = 2 
+	and ac._Object_key = sga._Gene_key 
+	and sga._Strain_key = sfa._Strain_key 
+	and sfa._StrainState_key <> 2 
+	and sfa._Facility_key = f._Facility_key''', None)
 	
-results = db.sql('''
-	select distinct accID, abbrevName 
-	from #imsrCounts 
-	where cType = 1
-	''', 'auto')
+# es cell line counts
+results = db.sql('select distinct accID, abbrevName from #imsrCounts where cType = 1', 'auto')
 es = {}
 for c in results:
 	accID = c['accID']
@@ -201,11 +242,8 @@ for c in results:
 	else:
 		es[accID].append(c['abbrevName'])
 	
-results = db.sql('''
-	select distinct accID, abbrevName 
-	from #imsrCounts 
-	where cType = 2
-	''', 'auto')
+# strain counts
+results = db.sql('select distinct accID, abbrevName from #imsrCounts where cType = 2', 'auto')
 strain = {}
 for c in results:
 	accID = c['accID']
@@ -214,17 +252,32 @@ for c in results:
 	else:
 		strain[accID].append(c['abbrevName'])
 	
+#
+# ready to print
+#
 results = db.sql('''
-	select distinct g.accID, g.version, g.term as tag, 
-	    cv.cellLine as mclID, cv.derivationName as mclLibrary, 
-	    c.parentCellLine, c.strain as parentCellLineStrain, 
-	    c.vector, cv.creator as mclCreator, g.alleleID, g.symbol, 
-	    g.name, g.alleleType, g.markerID, g.markerSymbol 
-	from #gt_seqs g, #cells c, ALL_Allele_CellLine ac, 
-	    ALL_Allele_CellLine_View cv 
-	where g._Allele_key = ac._Allele_key 
-	    and g._Allele_key = c._Allele_key 
-	    and ac._MutantCellLine_key = cv._MutantCellLine_key
+	select 
+	    g._Allele_key, 
+	    g.symbol, 
+	    g.name,
+	    g.alleleType,
+	    g.alleleID,
+	    m.markerID, 
+	    m.markersymbol,
+	    s.accID, 
+	    s.version, 
+	    s.tag, 
+	    cv.mclID,
+	    cv.parentCellLineStrain,
+	    cv.mclCreator,
+	    cv.vector, 
+	    cv.parentCellLine, 
+	    cv.mclLibrary
+	from #genetrap g 
+		LEFT OUTER JOIN #markers m on (g._Allele_key = m._Allele_key),
+	     #sequences s, #celllines cv
+	where g._Allele_key = s._Allele_key
+	      and g._Allele_key = cv._Allele_key
 	    ''', 'auto')
 
 for row in results:
@@ -233,10 +286,13 @@ for row in results:
 	
 	if row['alleleID'] in es:
 		esProviders = esProviders.union(set(es[row['alleleID']]))
+
 	if row['markerID'] in es:
 		esProviders = esProviders.union(set(es[row['markerID']]))
+
 	if row['alleleID'] in strain:
 		strainProviders = strainProviders.union(set(strain[row['alleleID']]))
+
 	if row['markerID'] in strain:
 		strainProviders = strainProviders.union(set(strain[row['markerID']]))
 		
@@ -254,7 +310,7 @@ for row in results:
         fp.write(row['name'] + TAB)
         fp.write(row['alleleType'] + TAB)
         fp.write(str(row['markerID']) + TAB)
-        fp.write(str(row['markerSymbol']) + TAB)
+        fp.write(str(row['markersymbol']) + TAB)
         fp.write(str(esString) + TAB)
         fp.write(str(strString) + TAB)
         fp.write(row['accID'] + '.' + row['version'] + TAB)
