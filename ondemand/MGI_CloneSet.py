8906fd94eea7f964ead24ca6799dbbb742fc651c
Sharon Giannatto <Sharon.Giannatto@jax.org>
2013-06-21 18:31:04 +0000

    removed check for DB_TYPE

--- ondemand/MGI_CloneSet.py
+++ ondemand/MGI_CloneSet.py
@@ -34,18 +34,7 @@ import os
 import string
 import mgi_utils
 import reportlib
-
-try:
-    if os.environ['DB_TYPE'] == 'postgres':
-        import pg_db
-        db = pg_db
-        db.setTrace()
-        db.setAutoTranslateBE()
-    else:
-        import db
-except:
-    import db
-
+import pg_db
 
 TAB = reportlib.TAB
 CRT = reportlib.CRT
