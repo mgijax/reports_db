45f44eef494432e33936dc30a7d789f9842f4870
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2020-03-21 09:58:20 -0400

    updated as test report for updating dependent libraries lib_py_report and lib_py_postgres

--- weekly/MGI_StrainTranslations.py
+++ weekly/MGI_StrainTranslations.py
@@ -1,20 +1,20 @@
-#!/usr/local/bin/python
-
 '''
 #
 # Report:
 #       Tab-delimited file
 #
-#	1. MGI Strain
-#	2. Literature Strain
+#       1. MGI Strain
+#       2. Literature Strain
 #
 # Usage:
 #       MGI_StrainTranslations.py
 #
 # History:
 #
-# lec	08/02/2005
-#	- Donna Maglott
+# sc    03/21/20 python 3 upgrade
+#
+# lec   08/02/2005
+#       - Donna Maglott
 #
 '''
  
@@ -39,15 +39,15 @@ db.useOneConnection(1)
 fp = reportlib.init(sys.argv[0], printHeading = None, outputdir = os.environ['REPORTOUTPUTDIR'])
 
 results = db.sql('''
-	select s.strain, t.badName 
-	from MGI_Translation t, PRB_Strain s 
-	where t._TranslationType_key = 1007 
-	and t._Object_key = s._Strain_key
-	''', 'auto')
+        select s.strain, t.badName 
+        from MGI_Translation t, PRB_Strain s 
+        where t._TranslationType_key = 1007 
+        and t._Object_key = s._Strain_key
+        ''', 'auto')
 
 for r in results:
 
-	fp.write(r['strain'] + TAB + r['badName'] + CRT)
+        fp.write(r['strain'] + TAB + r['badName'] + CRT)
 
-reportlib.finish_nonps(fp)	# non-postscript file
+reportlib.finish_nonps(fp)      # non-postscript file
 db.useOneConnection(0)
