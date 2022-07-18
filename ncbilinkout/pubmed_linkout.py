1dcf84f6d20a5d7c81929bb9e868e994044fb590
Lori Corbani <lori.corbani@jax.org>
2022-07-18 09:50:03 -0400

    wts2-704/Problem with MGI's latest Europe PMC external links upload

--- ncbilinkout/pubmed_linkout.py
+++ ncbilinkout/pubmed_linkout.py
@@ -47,6 +47,7 @@ fpLinkOut = reportlib.init(fileName, fileExt = '.xml', outputdir = os.environ['R
 
 count = 1
 
+fpLinkOut.write('<?xml version="1.0"?>' + CRT)
 fpLinkOut.write('<!DOCTYPE LinkSet PUBLIC "-//NLM//DTD LinkOut //EN" "https://www.ncbi.nlm.nih.gov/projects/linkout/doc/LinkOut.dtd"\n[' + CRT)
 fpLinkOut.write('<!ENTITY icon "' + os.environ['NCBILINKOUT_ICON'] + '">' + CRT)
 fpLinkOut.write('<!ENTITY base "' + os.environ['NCBILINKOUT_BASE_REF'] + '">' + CRT)
